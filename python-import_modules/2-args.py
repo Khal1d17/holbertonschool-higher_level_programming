aab6f4ffa01522dea34a5462eebdd3113cb790e2
Xalid <xalidlixalid056@gmail.com>
2025-06-16 16:06:26 +0000

    678

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,10 +3,10 @@ import sys
 if __name__ == "__main__":
     k = len(sys.argv)-1
     if k == 0:
-        print("0 arguments.")
+        print("0 argumentsi.")
     elif k == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(k))
     for i in range(1, k+1):
-        print("{}: {}".format(i, sys.argv)
+        print("{}: {}".format(i, sys.argv))
