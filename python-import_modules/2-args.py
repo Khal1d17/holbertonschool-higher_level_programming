6cb849f431ee045b8c7b0dfec9ac10ff4b64b9cb
Xalid <xalidlixalid056@gmail.com>
2025-06-16 16:00:46 +0000

    678

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif k == 1:
         print("1 arguments:")
     else:
-        print("{} arguments:" format.(k))
+        print("{} arguments:".format(k))
     for i in range(1, k+1):
         print("{}: {}".format(i, sys(argv))
