08ab45bebd33183dd14d4660dadd11f2fb8ae6bb
Xalid <xalidlixalid056@gmail.com>
2025-06-16 15:58:01 +0000

    678

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    k = len(sys(argv))-1
+    if k = 0:
+        print("0 arguments.")
+    elif k =1:
+        print("1 arguments:")
+    else:
+        print("{} arguments:" format.(k))
+    for i in range(1, k+1):
+        print("{}: {}".format(i, sys(argv))
