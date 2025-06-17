a92149ca11d2f5614bbae10531df53b4454513b3
xalid <xalidlixalid056@gmail.com>
2025-06-17 21:26:17 +0000

    Salam

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    k = []
     for i in my_list:
         if i % 2 == 0:
-            return True
+            k.append(True)
         else:
-            return False
+            k.append(False)
+    return k
