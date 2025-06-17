010a4b049b614aed5ce402de4c43564d1bdbcf0b
xalid <xalidlixalid056@gmail.com>
2025-06-17 20:12:23 +0000

    Salam

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,4 +2,4 @@
 def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
-    return "Length: {:d} - First character: {}".format(length, first)
+    return ("Length: {:d} - First character: {}".format(length, first))
