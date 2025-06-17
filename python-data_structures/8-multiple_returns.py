c0cc0d885d01670f9b9f8bd978008301e3111a5f
xalid <xalidlixalid056@gmail.com>
2025-06-17 20:09:01 +0000

    Salam

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    first_element = sentence[0]
+    return "Length: {:d} - First character: {}".format(length, first)
