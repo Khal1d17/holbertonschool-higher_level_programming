309b808b49cf777d48ece94cd305d3d427ca23c8
xalid <xalidlixalid056@gmail.com>
2025-06-17 20:55:43 +0000

    Salam

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     k = sorted(my_list, reversed=True)
-    if len(my_list) = 0:
+    if len(my_list) == 0:
         return None 
     else:
         return k[0]
