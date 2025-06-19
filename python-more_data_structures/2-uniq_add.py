94f5f41da9942f1c40ef3ab8b35b513c51c453b6
xalid <xalidlixalid056@gmail.com>
2025-06-19 12:35:26 +0000

    678

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,5 +3,4 @@ def uniq_add(my_list=[]):
     result = 0
     for i in my_list:
         result = result + i
-    result_int = int(result_int)
-    return ("Result: {:d}".format(result_int))
+    return int(result)
