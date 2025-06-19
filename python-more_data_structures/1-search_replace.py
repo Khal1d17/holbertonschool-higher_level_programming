bcefd382c7cc75edb73d6c67f951a2f662f2203a
xalid <xalidlixalid056@gmail.com>
2025-06-19 12:03:05 +0000

    678

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.replace(search, replace)
-    return new_list
+    return [replace if x == search else x for x in my_list]
