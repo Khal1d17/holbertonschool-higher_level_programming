532cbf15b3a122cef65a83f5b5cd9df611ac85ef
xalid <xalidlixalid056gmail.com>
2025-06-20 06:46:52 +0000

    678

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return max(a_dictionary)
+    if for i in a_dictionary.values():
+        return max(a_dictionary)
+    else:
+        return None
