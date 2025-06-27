e66d1f0db62f4fc3a3b39a67a74eb2398f3b1677
xalid <xalidlixalid056@gmail.com>
2025-06-27 12:04:33 +0000

    More classes

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,12 +6,12 @@ class Rectangle:
     """xalid"""
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """width qaytarir"""
+        '''Eni qaytarır.'''
         return self.__width
 
     @width.setter
@@ -24,3 +24,15 @@ class Rectangle:
         self.__width = value
 
     @property
+    def height(self):
+        '''Hündürlüyü qaytarır.'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''Hündürlüyü təyin edir.'''
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
